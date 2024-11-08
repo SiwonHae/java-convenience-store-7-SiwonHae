253f80eab4b2c859380a84d79b4d77e6401502a2
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-08 13:33:33 +0900

    refactor: 현재 날짜와 시간을 가져오는 방식 수정

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,10 +1,11 @@
 package store.model;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import java.time.LocalDate;
 
 public record Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
     private boolean isActive() {
-        LocalDate today = LocalDate.now();
+        LocalDate today = DateTimes.now().toLocalDate();
         return !today.isBefore(startDate) && !today.isAfter(endDate);
     }
 }
