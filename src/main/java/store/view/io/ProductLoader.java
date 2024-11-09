9a7d25a37a6bfd4c19af6c2237ee66e93e09891c
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-09 22:14:40 +0900

    refactor: 파일로부터 상품 객체 생성 로직 수정

--- src/main/java/store/view/io/ProductLoader.java
+++ src/main/java/store/view/io/ProductLoader.java
@@ -65,7 +65,6 @@ public class ProductLoader {
         Promotion promotion = null;
         if (!promotionName.equals("null")) {
             promotion = new Promotion(promotionName);
-            return new Product(name, price, quantity, promotion);
         }
         return new Product(name, price, quantity, promotion);
     }
